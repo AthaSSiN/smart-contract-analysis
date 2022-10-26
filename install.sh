72d66f981899011cd0c687d9f1d00943841d4f2c
AthaSSiN <athassin.asp@gmail.com>
2022-10-26 16:44:10 +0530

    update

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pip3 install -r requirements.txt
+pip install -r requirements.txt
 docker pull mythril/myth
 docker pull trailofbits/eth-security-toolbox
-docker build -t est2
+docker build -t est2 .
